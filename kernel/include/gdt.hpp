6d65d14e5e3e37f9770fcad4e9e11a3255e4d5f9
05032MB <36550385+05032MB@users.noreply.github.com>
2018-12-01 23:10:44 +0100

    nicer tss struct

--- kernel/include/gdt.hpp
+++ kernel/include/gdt.hpp
@@ -96,7 +96,7 @@ extern word KERNEL_DS;
 //nilX represents reserved bits which are set to 0
 struct tss_entry
 { 
- /*  word prev_task_link, nil1;
+   word prev_task_link, nil1;
    dword esp0;      
    word ss0, nil2;        
    dword esp1;      
@@ -121,34 +121,7 @@ struct tss_entry
    word fs, nil9;         
    word gs, nil10;         
    word ldtr, nil11;        
-   word T, iomba_offset; //T is debug trap(in reality 1 bit), iomba is I/O map base address field */
-     uint32_t prev_tss;   // The previous TSS - if we used hardware task switching this would form a linked list.
-   uint32_t esp0;       // The stack pointer to load when we change to kernel mode.
-   uint32_t ss0;        // The stack segment to load when we change to kernel mode.
-   uint32_t esp1;       // everything below here is unusued now.. 
-   uint32_t ss1;
-   uint32_t esp2;
-   uint32_t ss2;
-   uint32_t cr3;
-   uint32_t eip;
-   uint32_t eflags;
-   uint32_t eax;
-   uint32_t ecx;
-   uint32_t edx;
-   uint32_t ebx;
-   uint32_t esp;
-   uint32_t ebp;
-   uint32_t esi;
-   uint32_t edi;
-   uint32_t es;         
-   uint32_t cs;        
-   uint32_t ss;        
-   uint32_t ds;        
-   uint32_t fs;       
-   uint32_t gs;         
-   uint32_t ldt;      
-   uint16_t trap;
-   uint16_t iomap_base;
+   word T, iomba_offset; //T is debug trap(in reality 1 bit), iomba is I/O map base address field
 } __attribute__((packed));
 
 
